b3510bee7cf78f0cd95ce637a436c697d836bb25
CoRN <master@mastergpu08>
2018-03-18 22:05:13 +0100

    dataset name bugfix

--- code/models/model.py
+++ code/models/model.py
@@ -156,10 +156,10 @@ class One_Net_Model(Model):
                                                name='decoded_predictions')(x)
 	            self.model = K.Model(input=self.model.inputs,output=x)
 
-
+	
                 if dataset_name == 'TT100K_detection':
                     classes = ['i2','i4','i5','il100','il60','il80','io','ip','p10','p11','p12','p19','p23','p26','p27','p3','p5','p6','pg','ph4','ph4.5','ph5','pl100','pl120','pl20','pl30','pl40','pl5','pl50','pl60','pl70','pl80','pm20','pm30','pm55','pn','pne','po','pr40','w13','w32','w55','w57','w59','wo']
-                elif dataset_name == 'Udacity':
+                elif (dataset_name == 'Udacity')or(dataset_name == "Udacity_v2"):
                     classes = ['Car','Pedestrian','Truck']
                 else:
                     print("Error: Dataset not found!")
